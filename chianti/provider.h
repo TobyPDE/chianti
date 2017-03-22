c6a437ebdd0c26c02a9acd8c78492cbfd618322c
Toby Pohlen <tobias.pohlen@gmail.com>
2017-03-22 14:37:30 +0100

    Add std:: prefix for isnan #1

--- chianti/provider.h
+++ chianti/provider.h
@@ -263,7 +263,7 @@ namespace Chianti {
                             // Convert it to RGB
                             const int _i1 = 2 - i1;
                             imgs[i0 * imgsStrides[0] + _i1 * imgsStrides[1] + i2 * imgsStrides[2] + i3 * imgsStrides[3]] = static_cast<float>(img[i1]);
-                            if (isnan(imgs[i0 * imgsStrides[0] + _i1 * imgsStrides[1] + i2 * imgsStrides[2] + i3 * imgsStrides[3]])) {
+                            if (std::isnan(imgs[i0 * imgsStrides[0] + _i1 * imgsStrides[1] + i2 * imgsStrides[2] + i3 * imgsStrides[3]])) {
                                 imgs[i0 * imgsStrides[0] + _i1 * imgsStrides[1] + i2 * imgsStrides[2] + i3 * imgsStrides[3]] = 1.0f;
                             }
                         }
